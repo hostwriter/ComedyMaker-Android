13cb59a4aecfd61e7334d8d61713821fbe94fc50
Jesus Torres <jtorre40@msudenver.edu>
2020-09-27 01:52:37 -0600

    Rearranged methods to work better with unit tests

--- PersonalWorkRepo/PracticeApps/GPT3ComedyMaker2/app/src/main/java/com/projektekis/gpt3comedymaker/MainActivity.kt
+++ PersonalWorkRepo/PracticeApps/GPT3ComedyMaker2/app/src/main/java/com/projektekis/gpt3comedymaker/MainActivity.kt
@@ -6,43 +6,31 @@ import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    val defaultList = arrayListOf("Do I have a right to own a guitar? I don't.",
+        "I like to joke. I do iced iced iced ices.",
+        "I'll give you a list of the top 1% of the population who doesn't work two days a week, and " +
+                "a list of the worst 6% that doesn't work any more... and a great name.",
+        "Wont forget my last job. I can't do this anymore. I've lost my mind.")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Test list: This list will be replaced on app startup with data from the api
-        var testList = arrayListOf("Do I have a right to own a guitar? I don't.",
-            "I like to joke. I do iced iced iced ices.",
-            "I'll give you a list of the top 1% of the population who doesn't work two days a week, and a list of the worst 6% that doesn't work any more... and a great name.",
-            "Wont forget my last job. I can't do this anymore. I've lost my mind.")
-
-        var goodFeedbackList: ArrayList<String> = arrayListOf()
-
-
-        var jokeIterator = testList.iterator()
-
+        val goodFeedbackList: ArrayList<String> = arrayListOf()
         val downVoteBtn = findViewById<FloatingActionButton>(R.id.dislikeFloatingButton)
         val upVoteBtn = findViewById<FloatingActionButton>(R.id.likeFloatingButton)
         val shownJoke = findViewById<TextView>(R.id.textView)
 
-        fun setJokeIterator(jokeList: ArrayList<String>){
-            jokeIterator = if(jokeList.isEmpty()){
-                //Test list will become a default list in case incoming lists are be empty
-                testList.iterator()
-            }else {
-                jokeList.iterator()
-            }
-        }
+        //TODO (This will be replaced by incoming info from api)
+        var jokeIterator = setJokeIterator(defaultList)
 
-        fun sendFeedback(){
-            TODO("Send list of liked jokes back to the api")
-        }
 
         downVoteBtn.setOnClickListener{
             if(jokeIterator.hasNext()) {
                 shownJoke.text = jokeIterator.next()
             }else{
-                setJokeIterator(testList)
+                jokeIterator = setJokeIterator(defaultList)
                 shownJoke.text = jokeIterator.next()
             }
         }
@@ -53,12 +41,27 @@ class MainActivity : AppCompatActivity() {
                 shownJoke.text = jokeIterator.next()
 
             }else{
-                setJokeIterator(testList)
+                jokeIterator = setJokeIterator(defaultList)
                 goodFeedbackList.add(shownJoke.text.toString())
                 shownJoke.text = jokeIterator.next()
             }
         }
 
+    }
+    fun talkToApi(incomingJokes: ArrayList<String>): Boolean {
+        //TODO("Get info from api")
+        return incomingJokes.isNotEmpty()
+    }
+
+    fun sendFeedback(){
+        //TODO("Send list of liked jokes back to the api")
+    }
+
+    fun setJokeIterator(jokeList: ArrayList<String>): Iterator<String>{
+        if (jokeList.isEmpty())
+            return defaultList.iterator()
+        else
+            return jokeList.iterator()
     }
 
 }
--- PersonalWorkRepo/PracticeApps/GPT3ComedyMaker2/app/src/test/java/com/projektekis/gpt3comedymaker/MainActivityTest.kt
+++ PersonalWorkRepo/PracticeApps/GPT3ComedyMaker2/app/src/test/java/com/projektekis/gpt3comedymaker/MainActivityTest.kt
@@ -1,34 +1,54 @@
 package com.projektekis.gpt3comedymaker
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Assert
 import org.junit.Test
 
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.rules.TestRule
 
 class MainActivityTest {
+    var testAct = MainActivity()
+    val emptyList = arrayListOf<String>()
+    var testList = arrayListOf("joke1", "joke2", "joke3")
 
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
 
     @Test
     fun onCreate() {
+        //Assert main activity is the view
+    }
 
+    @Test
+    fun checkButtons(){
+        upvoteButtonWorking()
+        downvoteButtonWorking()
+
+    }
+
+    private fun downvoteButtonWorking() {
+        TODO("Not yet implemented")
+    }
+
+    private fun upvoteButtonWorking() {
+        TODO("Not yet implemented")
     }
 
     @Test
     fun checkList(){
         jokesListIsEmpty()
-        jokesListHasNext()
+        //jokesListHasNext()
     }
 
     private fun jokesListIsEmpty() {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
+        Assert.assertFalse(testAct.talkToApi(emptyList))
     }
 
     private fun jokesListHasNext() {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
+
     }
 
     @Test
@@ -38,11 +58,46 @@ class MainActivityTest {
     }
 
     private fun emptyJokeText() {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
     private fun textIsTooLong() {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
+    @Test
+    fun checkApiCommunication(){
+        noDataRetrieved()
+        apiDown()
+    }
+
+    private fun noDataRetrieved() {
+        //TODO("Not yet implemented")
+    }
+
+    private fun apiDown() {
+        //TODO("Not yet implemented")
+    }
+
+    @Test
+    fun checkIterator(){
+        iteratorHasNext()
+        listIsEmpty()
+
+    }
+
+    private fun iteratorHasNext() {
+        val testIter = testAct.setJokeIterator(testList)
+        Assert.assertEquals("joke1", testIter.next())
+        Assert.assertEquals("joke2", testIter.next())
+    }
+
+
+    private fun listIsEmpty() {
+        val testIter = testAct.setJokeIterator(emptyList)
+        val defaultIter = testAct.defaultList.iterator()
+        Assert.assertEquals(defaultIter.next(), testIter.next())
+        Assert.assertEquals(defaultIter.next(), testIter.next())
+
+    }
 }
