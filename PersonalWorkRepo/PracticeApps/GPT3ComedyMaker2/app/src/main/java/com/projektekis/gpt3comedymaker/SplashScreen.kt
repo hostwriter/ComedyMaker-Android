5d50e80bc33b6466032d14859b15aaae2e299133
Jesus Torres <jtorre40@msudenver.edu>
2020-10-01 16:25:42 -0600

    Created Splash Screen

---
+++ PersonalWorkRepo/PracticeApps/GPT3ComedyMaker2/app/src/main/java/com/projektekis/gpt3comedymaker/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.projektekis.gpt3comedymaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        splash_mic.alpha = 0f
+        splash_mic.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
